adeb4eb2075edef7186deaa664ef8b026097b4d6
m33shoq <mishadyrov@gmail.com>
2024-03-22 19:38:19 +0200

    make english locale optional

--- WeakAuras/WeakAuras.lua
+++ WeakAuras/WeakAuras.lua
@@ -1364,6 +1364,10 @@ loadedFrame:SetScript("OnEvent", function(self, event, addon)
       db.migrationCutoff = db.migrationCutoff or 730
       db.historyCutoff = db.historyCutoff or 730
 
+      if not db.disableRuLocale and WeakAuras.LoadLocale_ruRU then
+        WeakAuras.LoadLocale_ruRU()
+      end
+
       Private.SyncParentChildRelationships();
       local isFirstUIDValidation = db.dbVersion == nil or db.dbVersion < 26;
       Private.ValidateUniqueDataIds(isFirstUIDValidation);
