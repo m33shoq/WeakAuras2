0e1ae6abc2b1fd007e10822c006e6b9263eae237
m33shoq <mishadyrov@gmail.com>
2025-07-11 20:57:36 +0300

    add `rg_custom_error_handler` as wasync field

--- WeakAuras/Types.lua
+++ WeakAuras/Types.lua
@@ -3907,6 +3907,7 @@ Private.update_categories = {
       "exrtLastSender",
       "exrtUpdateConfig",
       "exrtDefaultLoadNever",
+      "rg_custom_error_handler",
     },
     default = false,
     label = "WASync settings",
