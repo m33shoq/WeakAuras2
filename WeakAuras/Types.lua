2eb9ea0bbbd7f9f8ec6bea6618535d8ced9cacb5
m33shoq <mishadyrov@gmail.com>
2025-08-08 18:43:53 +0300

    add `exrtToSend` as WASync field

--- WeakAuras/Types.lua
+++ WeakAuras/Types.lua
@@ -3908,6 +3908,7 @@ Private.update_categories = {
       "exrtUpdateConfig",
       "exrtDefaultLoadNever",
       "rg_custom_error_handler",
+      "exrtToSend",
     },
     default = false,
     label = "WASync settings",
